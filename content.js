03683fe4e03d9705f92dd34e329f7f2d6b8a1742
hakimm77 <hakimboum406@gmail.com>
2022-07-13 15:09:32 +0200

    finished the extension up

---
+++ content.js
@@ -0,0 +1,29 @@
+const message = document.getElementById(
+  "ContentPlaceHolder1_txtSignedMessageReadonly"
+).value;
+
+const signature = document.getElementById(
+  "ContentPlaceHolder1_txtSignatureHash"
+).value;
+
+const sendMessageDiscord = async () => {
+  if (signature && message) {
+    await fetch(
+      `http://localhost:8080/test-message?username=<VeryCoolProgrammer />&signature=${signature}&message=${message}`
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+
+    alert("Message is sent !");
+  }
+};
+
+window.addEventListener("load", () => {
+  const sendDiscord = confirm(
+    "Do you want to send this message in the discord server ?"
+  );
+
+  if (sendDiscord) {
+    sendMessageDiscord();
+  }
+});
